7edc7ec58cbe6e57c2558eecce861450eba03862
Caio <caio-cavalcanti@hotmail.com>
2021-08-15 02:48:10 -0300

    fim mercenario

--- js/mercenario.js
+++ js/mercenario.js
@@ -8,7 +8,7 @@
 // function teste()
 setTimeout(function() {
     window.onload = escolha1()
-}, 500);
+}, 1000);
 
 
 function escolha1() {
@@ -24,16 +24,106 @@ function escolha1() {
     }
     if (escolhaMerce1 == 1) {
         document.getElementById('inicio').style.display = 'none';
+        document.getElementById('gameover').style.display = 'flex';
 
-        return document.getElementById('gameover').style.display = 'block';
+        return setTimeout(function() {
+            window.onload = window.location.href = 'index.html'
+        }, 5000);
+
+
 
     } else if (escolhaMerce1 == 2) {
         document.getElementById('inicio').style.display = 'none';
+        document.getElementById('gameover').style.display = 'flex';
 
-        return document.getElementById('gameover').style.display = 'block';
+        return setTimeout(function() {
+            window.onload = window.location.href = 'index.html'
+        }, 5000);
 
     } else if (escolhaMerce1 == 3) {
+
         document.getElementById('inicio').style.display = 'none';
-        return document.getElementById('telhado').style.display = 'block';
+        document.getElementById('telhado').style.display = 'flex'
+
+        return setTimeout(function() {
+            window.onload = escolhamercenario2()
+        }, 5000);
+
     }
+}
+
+
+function escolhamercenario2() {
+
+    while (true) {
+        var escolha2 = prompt(`Qual caminho voce gostaria de seguir ?
+        1 - Nao Ajuda
+        2 - Ajuda
+        3 - Corre
+        `)
+        if (escolha2 == 1 || escolha2 == 2 || escolha2 == 3)
+            break
+
+    }
+    if (escolha2 == 1) {
+        document.getElementById('telhado').style.display = 'none';
+        document.getElementById('gameover').style.display = 'flex';
+
+        return setTimeout(function() {
+            window.onload = window.location.href = 'index.html'
+        }, 5000);
+
+    } else if (escolha2 == 2) {
+        document.getElementById('telhado').style.display = 'none'
+        document.getElementById('laboratorio').style.display = "flex"
+
+
+        return setTimeout(function() {
+            window.onload = escolhamercenario3()
+        }, 5000);
+
+
+    } else if (escolha2 == 3) {
+        document.getElementById('telhado').style.display = 'none';
+        document.getElementById('gameover').style.display = 'flex';
+
+        return setTimeout(function() {
+            window.onload = window.location.href = 'index.html'
+        }, 5000);
+
+    }
+
+}
+
+
+function escolhamercenario3() {
+
+    while (true) {
+        var escolha3 = prompt(`Qual caminho vc quer seguir?
+        1 - A armadura é minha eu ralei eu mereço!
+        2 - Vou divir com meu parça, eu ajudei ele justo a gente sair juntos!`)
+        if (escolha3 == 1 || escolha3 == 2)
+            break
+    }
+    if (escolha3 == 1) {
+        document.getElementById('laboratorio').style.display = "none";
+        document.getElementById('fimbom').style.display = "flex";
+        return setTimeout(function() {
+            window.onload = window.location.href = 'index.html'
+        }, 5000);
+
+
+    } else if (escolha3 == 2) {
+        document.getElementById('laboratorio').style.display = "none";
+        document.getElementById('fimruim').style.display = "flex";
+        return setTimeout(function() {
+            window.onload = window.location.href = 'index.html'
+        }, 5000);
+
+    }
+
+
+
+
+
 }
